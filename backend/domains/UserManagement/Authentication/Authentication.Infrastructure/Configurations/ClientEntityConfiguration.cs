695799de6d14f02b7fc211b96c38c408e292b3b4
Yauheni Charniauski <chernyavskij.z@mail.ru>
2025-09-28 19:09:57 +0300

    Add ClientEntityConfiguration

---
+++ backend/domains/UserManagement/Authentication/Authentication.Infrastructure/Configurations/ClientEntityConfiguration.cs
@@ -0,0 +1,39 @@
+using ch4rniauski.BankApp.Authentication.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ch4rniauski.BankApp.Authentication.Infrastructure.Configurations;
+
+public class ClientEntityConfiguration : IEntityTypeConfiguration<ClientEntity>
+{
+    public void Configure(EntityTypeBuilder<ClientEntity> builder)
+    {
+        builder.HasKey(c => c.Id);
+        
+        builder
+            .Property(u => u.FirstName)
+            .IsRequired()
+            .HasMaxLength(50);
+        
+        builder
+            .Property(u => u.LastName)
+            .IsRequired()
+            .HasMaxLength(100);
+        
+        builder
+            .Property(c => c.Email)
+            .IsRequired();
+
+        builder
+            .Property(c => c.IsEmailVerified)
+            .IsRequired();
+        
+        builder
+            .Property(c => c.PhoneNumber)
+            .IsRequired();
+        
+        builder
+            .Property(c => c.PasswordHash)
+            .IsRequired(false);
+    }
+}
