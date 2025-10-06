4aa7db3ac1c35f4289deedace74ba7d9e21bb7a6
Yauheni Charniauski <chernyavskij.z@mail.ru>
2025-10-06 20:43:22 +0300

    Add Context DI configuration

--- backend/domains/UserManagement/CreditCards/CreditCards.Api/Program.cs
+++ backend/domains/UserManagement/CreditCards/CreditCards.Api/Program.cs
@@ -1,6 +1,10 @@
+using ch4rniauski.BankApp.CreditCards.Infrastructure.Extensions.DependencyInjectionExtensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddCreditCardContext(builder.Configuration);
 
 var app = builder.Build();
 
