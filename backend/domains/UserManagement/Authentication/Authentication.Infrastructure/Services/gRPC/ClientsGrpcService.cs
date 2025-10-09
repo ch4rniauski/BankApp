086e26055188f56d7f205da43bd321eff5143564
Yauheni Charniauski <chernyavskij.z@mail.ru>
2025-10-09 16:09:05 +0300

    Implement CheckIfClientExists gRPC method

--- backend/domains/UserManagement/Authentication/Authentication.Infrastructure/Services/gRPC/ClientsGrpcService.cs
+++ backend/domains/UserManagement/Authentication/Authentication.Infrastructure/Services/gRPC/ClientsGrpcService.cs
@@ -1,13 +1,34 @@
+using ch4rniauski.BankApp.Authentication.Application.Extensions;
+using ch4rniauski.BankApp.Authentication.Application.UseCases.Queries.Client;
 using ch4rniauski.BankApp.Authentication.Grpc;
 using Grpc.Core;
+using MediatR;
 
 namespace ch4rniauski.BankApp.Authentication.Infrastructure.Services.gRPC;
 
 public sealed class ClientsGrpcService : ClientsService.ClientsServiceBase
 {
+    private readonly IMediator _mediator;
+
+    public ClientsGrpcService(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
     public override async Task<CheckIfClientExistsResponse> CheckIfClientExists(CheckIfClientExistsRequest request, ServerCallContext context)
     {
+        var query = new CheckIfClientExistsQuery(request.ClientId);
         
-        return new CheckIfClientExistsResponse();
+        var result = await _mediator.Send(query, context.CancellationToken);
+
+        return result.Match(
+            onSuccess: _ => new CheckIfClientExistsResponse
+            {
+                DoesExist = true
+            },
+            onFailure: _ => new CheckIfClientExistsResponse
+            {
+                DoesExist = false
+            });
     }
 }
