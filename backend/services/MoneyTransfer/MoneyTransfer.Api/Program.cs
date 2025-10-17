d989d31c4ecd54c485e3f8d5b4874728f40dd25c
Yauheni Charniauski <chernyavskij.z@mail.ru>
2025-10-17 18:54:55 +0300

    Add DI configuration

--- backend/services/MoneyTransfer/MoneyTransfer.Api/Program.cs
+++ backend/services/MoneyTransfer/MoneyTransfer.Api/Program.cs
@@ -1,6 +1,10 @@
+using ch4rniauski.BankApp.MoneyTransfer.Infrastructure.Extensions.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddMoneyTransferConfiguration(builder.Configuration);
 
 var app = builder.Build();
 
--- backend/services/MoneyTransfer/MoneyTransfer.Infrastructure/Extensions/DependencyInjection/ServiceCollectionExtensions.cs
+++ backend/services/MoneyTransfer/MoneyTransfer.Infrastructure/Extensions/DependencyInjection/ServiceCollectionExtensions.cs
@@ -1,6 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
 namespace ch4rniauski.BankApp.MoneyTransfer.Infrastructure.Extensions.DependencyInjection;
 
 public static class ServiceCollectionExtensions
 {
-    
+    public static void AddMoneyTransferConfiguration(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<MoneyTransferContext>(opt =>
+        {
+            opt.UseNpgsql(configuration.GetConnectionString("PostgresMoneyTransfer"));
+        });
+    }
 }
