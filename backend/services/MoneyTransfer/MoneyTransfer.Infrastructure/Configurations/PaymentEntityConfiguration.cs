9b2c277997a948ce8eb84e344f01703040e78da6
Yauheni Charniauski <chernyavskij.z@mail.ru>
2025-10-17 18:50:43 +0300

    Add PaymentEntityConfiguration

---
+++ backend/services/MoneyTransfer/MoneyTransfer.Infrastructure/Configurations/PaymentEntityConfiguration.cs
@@ -0,0 +1,63 @@
+using ch4rniauski.BankApp.MoneyTransfer.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ch4rniauski.BankApp.MoneyTransfer.Infrastructure.Configurations;
+
+public sealed class PaymentEntityConfiguration : IEntityTypeConfiguration<PaymentEntity>
+{
+    public void Configure(EntityTypeBuilder<PaymentEntity> builder)
+    {
+        builder
+            .HasKey(p => p.Id);
+
+        builder
+            .Property(p => p.Currency)
+            .HasMaxLength(3)
+            .IsRequired();
+
+        builder
+            .Property(p => p.Amount)
+            .IsRequired();
+        
+        builder
+            .Property(p => p.SenderCardLast4)
+            .IsFixedLength()
+            .HasMaxLength(4)
+            .IsRequired();
+        
+        builder
+            .Property(p => p.ReceiverCardLast4)
+            .IsFixedLength()
+            .HasMaxLength(4)
+            .IsRequired();
+
+        builder
+            .Property(p => p.SenderId)
+            .IsRequired();
+        
+        builder
+            .Property(p => p.ReceiverId)
+            .IsRequired();
+        
+        builder
+            .Property(p => p.Description)
+            .HasMaxLength(150)
+            .IsRequired(false);
+        
+        builder
+            .Property(p => p.Status)
+            .HasConversion<string>()
+            .HasMaxLength(15)
+            .IsRequired();
+        
+        builder
+            .Property(p => p.ProcessedAt)
+            .IsRequired();
+
+        builder
+            .ToTable(t => t.HasCheckConstraint(
+                "CK_Payments_Amount_Positive",
+                $"[{nameof(PaymentEntity.Amount)}] > 0"));
+    }
+}
---
+++ backend/services/MoneyTransfer/MoneyTransfer.Infrastructure/Extensions/DependencyInjection/ServiceCollectionExtensions.cs
@@ -0,0 +1,6 @@
+namespace ch4rniauski.BankApp.MoneyTransfer.Infrastructure.Extensions.DependencyInjection;
+
+public static class ServiceCollectionExtensions
+{
+    
+}
