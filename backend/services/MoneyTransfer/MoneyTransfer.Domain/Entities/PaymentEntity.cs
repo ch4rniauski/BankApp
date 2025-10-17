b0a60d452f970f41dbae0f3a7321c62f161e2ce0
Yauheni Charniauski <chernyavskij.z@mail.ru>
2025-10-17 18:01:24 +0300

    Add PaymentEntity

---
+++ backend/services/MoneyTransfer/MoneyTransfer.Domain/Entities/PaymentEntity.cs
@@ -0,0 +1,26 @@
+using MoneyTransfer.Domain.Enums;
+
+namespace MoneyTransfer.Domain.Entities;
+
+public class PaymentEntity
+{
+    public Guid Id { get; set; }
+
+    public string Currency { get; set; } = string.Empty;
+    
+    public decimal Amount { get; set; }
+    
+    public string SenderCardLast4 { get; set; } = string.Empty;
+    
+    public string ReceiverCardLast4 { get; set; } = string.Empty;
+
+    public Guid SenderId { get; set; }
+    
+    public Guid ReceiverId { get; set; }
+
+    public string? Description { get; set; }
+    
+    public PaymentStatusEnum Status { get; set; } = PaymentStatusEnum.Pending;
+
+    public DateTime ProcessedAt { get; set; }
+}
---
+++ backend/services/MoneyTransfer/MoneyTransfer.Domain/Enums/PaymentStatusEnum.cs
@@ -0,0 +1,10 @@
+namespace MoneyTransfer.Domain.Enums;
+
+public enum PaymentStatusEnum
+{
+    Pending = 0,
+    Success = 1,
+    Failed = 2,
+    Canceled = 3,
+    Refunded = 4
+}
