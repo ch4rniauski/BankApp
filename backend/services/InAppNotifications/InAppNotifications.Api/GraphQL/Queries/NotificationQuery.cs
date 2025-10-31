de1ca12c16aecdf2f90f45c4b047b13c5f18252c
Yauheni Charniauski <chernyavskij.z@mail.ru>
2025-10-31 20:54:08 +0300

    Add NotificationQuery

---
+++ backend/services/InAppNotifications/InAppNotifications.Api/GraphQL/Queries/NotificationQuery.cs
@@ -0,0 +1,6 @@
+namespace ch4rniauski.BankApp.InAppNotifications.Api.GraphQL.Queries;
+
+public sealed class NotificationQuery
+{
+    
+}
--- backend/services/InAppNotifications/InAppNotifications.Api/Program.cs
+++ backend/services/InAppNotifications/InAppNotifications.Api/Program.cs
@@ -1,9 +1,13 @@
+using ch4rniauski.BankApp.InAppNotifications.Api.GraphQL.Queries;
 using ch4rniauski.BankApp.InAppNotifications.Infrastructure.MongoDb.BsonClassMaps;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
+builder.Services
+    .AddGraphQLServer()
+    .AddQueryType<NotificationQuery>();
 
 BsonClassMapRegistry.RegisterBsonClassMaps();
 
@@ -14,5 +18,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.MapGraphQL();
 
 await app.RunAsync();
