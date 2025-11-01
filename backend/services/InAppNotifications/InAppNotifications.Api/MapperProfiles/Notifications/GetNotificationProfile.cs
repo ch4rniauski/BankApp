a4e3e19cf5e14f7168cad0f3780c6854c73fd391
Yauheni Charniauski <chernyavskij.z@mail.ru>
2025-11-01 18:43:24 +0300

    Add GetNotificationById

---
+++ backend/services/InAppNotifications/InAppNotifications.Api/MapperProfiles/Notifications/GetNotificationProfile.cs
@@ -0,0 +1,33 @@
+using AutoMapper;
+using ch4rniauski.BankApp.InAppNotifications.Api.GraphQL.Types;
+using ch4rniauski.BankApp.InAppNotifications.Application.DTO.Responses.Notifications;
+using ch4rniauski.BankApp.InAppNotifications.Domain.Entities;
+
+namespace ch4rniauski.BankApp.InAppNotifications.Api.MapperProfiles.Notifications;
+
+internal sealed class GetNotificationProfile : Profile
+{
+    public GetNotificationProfile()
+    {
+        CreateMap<NotificationEntity, GetNotificationByIdResponseDto>()
+            .ConstructUsing(src => new GetNotificationByIdResponseDto(
+                src.Id,
+                src.Title,
+                src.Content,
+                src.IsRead));
+
+        CreateMap<GetNotificationByIdResponseDto, NotificationType>()
+            .ForMember(
+                dest => dest.Id,
+                opt => opt.MapFrom(src => src.Id))
+            .ForMember(
+                dest => dest.Title,
+                opt => opt.MapFrom(src => src.Title))
+            .ForMember(
+                dest => dest.Content,
+                opt => opt.MapFrom(src => src.Content))
+            .ForMember(
+                dest => dest.IsRead,
+                opt => opt.MapFrom(src => src.IsRead));
+    }
+}
