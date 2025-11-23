d0aa3e22dbfc1070f3b7628b7747289eec27bc4f
Yauheni Charniauski <chernyavskij.z@mail.ru>
2025-11-23 14:16:06 +0300

    Move authentication to gateway

--- backend/services/ApiGateway/ApiGateway.Api/Program.cs
+++ backend/services/ApiGateway/ApiGateway.Api/Program.cs
@@ -1,9 +1,28 @@
+using ch4rniauski.BankApp.ApiGateway.Application.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
+builder.Services.AddCors(opt =>
+    opt.AddDefaultPolicy(policy =>
+    {
+        policy
+            .WithOrigins("http://localhost:4200")
+            .AllowAnyHeader()
+            .AllowCredentials()
+            .AllowAnyMethod();
+    }));
+
+builder.Services.AddJwtConfiguration(builder.Configuration);
+
 var app = builder.Build();
+
+app.UseCors();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapReverseProxy();
 
--- backend/services/Authentication/Authentication.Application/Extensions/ServiceCollectionExtensions.cs
+++ backend/services/Authentication/Authentication.Application/Extensions/ServiceCollectionExtensions.cs
@@ -13,22 +13,6 @@ public static class ServiceCollectionExtensions
 {
     public static void AddJwtConfiguration(this IServiceCollection services, IConfiguration configuration)
     {
-        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-            .AddJwtBearer(opt =>
-            {
-                opt.TokenValidationParameters = new TokenValidationParameters
-                {
-                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JwtSettings:SecurityKey"]!)),
-                    ValidateIssuerSigningKey = true,
-                    ValidateLifetime = true,
-                    ValidateAudience = false,
-                    ValidateIssuer = false,
-                    ClockSkew = TimeSpan.Zero
-                };
-            });
-        
-        services.AddAuthorization();
-
         services.AddScoped<ITokenProvider, JwtTokenProvider>();
         
         services.Configure<JwtSettings>(configuration.GetSection("JwtSettings"));
