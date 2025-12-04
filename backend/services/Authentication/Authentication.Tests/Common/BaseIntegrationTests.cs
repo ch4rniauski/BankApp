ee5be7b3bf29dc5b03787f3040e4506223a51091
Yauheni Charniauski <chernyavskij.z@mail.ru>
2025-12-04 20:38:01 +0300

    Add BaseIntegrationTests

--- backend/services/Authentication/Authentication.Tests/Common/BaseIntegrationTests.cs
+++ backend/services/Authentication/Authentication.Tests/Common/BaseIntegrationTests.cs
@@ -1,3 +1,5 @@
+using ch4rniauski.BankApp.Authentication.Infrastructure;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 using Xunit;
 
@@ -6,9 +8,14 @@ namespace ch4rniauski.BankApp.Authentication.Tests.Common;
 internal abstract class BaseIntegrationTests : IClassFixture<AuthenticationWebAppFactory>
 {
     protected readonly IServiceScope ServiceScope;
+    protected readonly AuthenticationContext DbContext;
+    protected readonly IMediator Mediator;
     
     protected BaseIntegrationTests(AuthenticationWebAppFactory factory)
     {
         ServiceScope = factory.Services.CreateScope();
+
+        DbContext = ServiceScope.ServiceProvider.GetRequiredService<AuthenticationContext>();
+        Mediator = ServiceScope.ServiceProvider.GetRequiredService<IMediator>();
     }
 }
