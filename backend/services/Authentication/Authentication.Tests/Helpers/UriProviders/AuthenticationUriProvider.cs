74d7253efbbd57c58fb3eb5e3b2929d21e0f4df1
Yauheni Charniauski <chernyavskij.z@mail.ru>
2025-12-06 11:41:47 +0300

    Add DeleteClient_ReturnsOk

--- backend/services/Authentication/Authentication.Tests/Helpers/UriProviders/AuthenticationUriProvider.cs
+++ backend/services/Authentication/Authentication.Tests/Helpers/UriProviders/AuthenticationUriProvider.cs
@@ -2,8 +2,11 @@ namespace ch4rniauski.BankApp.Authentication.Tests.Helpers.UriProviders;
 
 internal static class AuthenticationUriProvider
 {
-    private const string BaseUri = "api/clients/";
+    private const string BaseUri = "api/clients";
 
     public static string GetRegisterClientUri()
         => $"{BaseUri}";
+    
+    public static string GetDeleteClientUri(Guid clientId)
+        => $"{BaseUri}/{clientId}";
 }
---
+++ backend/services/Authentication/Authentication.Tests/Tests/IntergrationTests/Clients/DeleteClientIntegrationTests.cs
@@ -0,0 +1,40 @@
+using System.Net;
+using System.Net.Http.Json;
+using ch4rniauski.BankApp.Authentication.Tests.Common;
+using ch4rniauski.BankApp.Authentication.Tests.Helpers.DataProviders;
+using ch4rniauski.BankApp.Authentication.Tests.Helpers.UriProviders;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+namespace ch4rniauski.BankApp.Authentication.Tests.Tests.IntergrationTests.Clients;
+
+public sealed class DeleteClientIntegrationTests : BaseIntegrationTests
+{
+    public DeleteClientIntegrationTests(AuthenticationWebAppFactory factory) : base(factory)
+    {
+    }
+    
+    [Fact]
+    public async Task DeleteClient_ReturnsOk()
+    {
+        // Arrange
+        var clientEntity = ClientDataProvider.GenerateClientEntity();
+
+        var uri = AuthenticationUriProvider.GetDeleteClientUri(clientEntity.Id);
+
+        // Act
+        await DbContext.Database.EnsureDeletedAsync();
+        await DbContext.Database.EnsureCreatedAsync();
+        
+        await DbContext.Clients.AddAsync(clientEntity);
+        await DbContext.SaveChangesAsync();
+        
+        var response = await HttpClient.DeleteAsync(uri);
+
+        var client = await DbContext.Clients.FirstOrDefaultAsync(c => c.Id == clientEntity.Id);
+
+        // Assert
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        Assert.Null(client);
+    }
+}
